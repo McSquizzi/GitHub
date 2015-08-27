4ea7a7cabef407af8a284ad469c0327246c3df0f
McSquizzi <domenshik@gmail.com>
2015-08-27 09:11:26 +0300

    first commit

---
+++ TextRed/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace TextRed
+{
+    partial class frmTextRedactor
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlTools = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnPaste = new System.Windows.Forms.Button();
+            this.btnCut = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.txtbTextSite = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pnlTools.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlTools
+            // 
+            this.pnlTools.Controls.Add(this.button3);
+            this.pnlTools.Controls.Add(this.button2);
+            this.pnlTools.Controls.Add(this.button1);
+            this.pnlTools.Controls.Add(this.textBox1);
+            this.pnlTools.Controls.Add(this.comboBox1);
+            this.pnlTools.Controls.Add(this.btnPaste);
+            this.pnlTools.Controls.Add(this.btnCut);
+            this.pnlTools.Controls.Add(this.btnCopy);
+            this.pnlTools.Controls.Add(this.menuStrip2);
+            this.pnlTools.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTools.Location = new System.Drawing.Point(0, 24);
+            this.pnlTools.Name = "pnlTools";
+            this.pnlTools.Size = new System.Drawing.Size(778, 100);
+            this.pnlTools.TabIndex = 0;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(195, 65);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(28, 25);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Ч";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(161, 65);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(28, 25);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "К";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(127, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(28, 25);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Ж";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(125, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(125, 6);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedValueChanged += new System.EventHandler(this.comboBox1_SelectedValueChanged);
+            // 
+            // btnPaste
+            // 
+            this.btnPaste.Location = new System.Drawing.Point(71, 39);
+            this.btnPaste.Name = "btnPaste";
+            this.btnPaste.Size = new System.Drawing.Size(46, 22);
+            this.btnPaste.TabIndex = 2;
+            this.btnPaste.Text = "Paste";
+            this.btnPaste.UseVisualStyleBackColor = true;
+            this.btnPaste.Click += new System.EventHandler(this.btnPaste_Click);
+            // 
+            // btnCut
+            // 
+            this.btnCut.Location = new System.Drawing.Point(71, 6);
+            this.btnCut.Name = "btnCut";
+            this.btnCut.Size = new System.Drawing.Size(46, 22);
+            this.btnCut.TabIndex = 1;
+            this.btnCut.Text = "Cut";
+            this.btnCut.UseVisualStyleBackColor = true;
+            this.btnCut.Click += new System.EventHandler(this.btnCut_Click);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(7, 6);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(58, 84);
+            this.btnCopy.TabIndex = 0;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(778, 24);
+            this.menuStrip2.TabIndex = 10;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // txtbTextSite
+            // 
+            this.txtbTextSite.Location = new System.Drawing.Point(71, 130);
+            this.txtbTextSite.Name = "txtbTextSite";
+            this.txtbTextSite.Size = new System.Drawing.Size(663, 489);
+            this.txtbTextSite.TabIndex = 1;
+            this.txtbTextSite.Text = "";
+            this.txtbTextSite.MouseClick += new System.Windows.Forms.MouseEventHandler(this.txtbTextSite_MouseClick);
+            this.txtbTextSite.MouseDown += new System.Windows.Forms.MouseEventHandler(this.txtbTextSite_MouseDown);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(778, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.loadToolStripMenuItem.Text = "Load";
+            this.loadToolStripMenuItem.Click += new System.EventHandler(this.loadToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveAsToolStripMenuItem.Text = "Save as...";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // frmTextRedactor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 641);
+            this.Controls.Add(this.txtbTextSite);
+            this.Controls.Add(this.pnlTools);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "frmTextRedactor";
+            this.Text = "Form1";
+            this.pnlTools.ResumeLayout(false);
+            this.pnlTools.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlTools;
+        private System.Windows.Forms.RichTextBox txtbTextSite;
+        private System.Windows.Forms.Button btnPaste;
+        private System.Windows.Forms.Button btnCut;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+    }
+}
+
---
+++ TextRed/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextRed
+{
+    public partial class frmTextRedactor : Form
+    {
+        public frmTextRedactor()
+        {
+            InitializeComponent();
+            foreach (FontFamily font in System.Drawing.FontFamily.Families)
+            {
+                fontName.Add(font.Name);
+
+            }
+            comboBox1.DataSource = fontName;
+
+            textBox1.Text = Font.Size.ToString();
+        }
+
+        //private void button1_MouseUp(object sender, MouseEventArgs e)
+        //{
+        //    FontDialog dlg = new FontDialog();
+        //    if (DialogResult.OK == dlg.ShowDialog())
+        //    {
+        //       txtbTextSite.Font = dlg.Font;
+
+
+        //    }
+        //}
+
+
+        private void txtbTextSite_MouseClick(object sender, MouseEventArgs e)
+        {
+            //if (e.Button == MouseButtons.Right)
+            //{
+            //    FontDialog dlg = new FontDialog();
+            //    if (DialogResult.OK == dlg.ShowDialog())                          не реагирует
+            //    {
+            //        txtbTextSite.SelectionFont = dlg.Font;
+            //    }
+
+            //}
+        }
+
+        private void txtbTextSite_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                FontDialog dlg = new FontDialog();
+                if (DialogResult.OK == dlg.ShowDialog())
+                {
+                    txtbTextSite.SelectionFont = dlg.Font;
+                }
+
+            }
+        }
+
+
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            txtTmp = txtbTextSite.SelectedText;
+        }
+
+        private void btnCut_Click(object sender, EventArgs e)
+        {
+            txtTmp = txtbTextSite.SelectedText;
+            txtbTextSite.SelectedText = null;
+        }
+
+        private void btnPaste_Click(object sender, EventArgs e)
+        {
+            if (txtTmp != null)
+            {
+                txtbTextSite.SelectedText = txtTmp;
+                txtTmp = null;
+            }
+        }
+        List<string> fontName = new List<string>();
+        private string txtTmp = null;
+        private string customPass = null;
+
+        private void comboBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            if (txtbTextSite.SelectedText != null)
+            {
+                Font frm = new Font(comboBox1.Text, Font.Size);
+                txtbTextSite.SelectionFont = frm;
+            }
+            else
+            {
+                Font frm = new Font(comboBox1.Text, Font.Size);
+                Font = frm;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Enter)
+            {
+
+                if (textBox1.Text != null && textBox1.Text != "")
+                {
+                    if (txtbTextSite.SelectedText != null)
+                    {
+                        Font frm = new Font(Font.FontFamily, float.Parse(textBox1.Text));
+                        txtbTextSite.SelectionFont = frm;
+                    }
+                    else
+                    {
+                        Font frm = new Font(Font.FontFamily, float.Parse(textBox1.Text));
+                        Font = frm;
+                    }
+
+                }
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtbTextSite.SelectedText != null)
+            {
+                if (txtbTextSite.SelectionFont.Bold)
+                {
+                    txtbTextSite.SelectionFont = new Font(txtbTextSite.SelectionFont, FontStyle.Regular);
+                }
+                else
+                {
+                    txtbTextSite.SelectionFont = new Font(txtbTextSite.SelectionFont, FontStyle.Bold);
+                }
+            }
+            else
+            {
+                if (Font.Bold)
+                {
+                    Font = new Font(txtbTextSite.SelectionFont, FontStyle.Regular);
+                }
+                else
+                {
+                    Font = new Font(txtbTextSite.SelectionFont, FontStyle.Bold);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (txtbTextSite.SelectedText != null)
+            {
+                if (txtbTextSite.SelectionFont.Italic)
+                {
+                    txtbTextSite.SelectionFont = new Font(txtbTextSite.SelectionFont, FontStyle.Regular);
+                }
+                else
+                {
+                    txtbTextSite.SelectionFont = new Font(txtbTextSite.SelectionFont, FontStyle.Italic);
+                }
+            }
+            else
+            {
+                if (Font.Italic)
+                {
+                    Font = new Font(txtbTextSite.SelectionFont, FontStyle.Regular);
+                }
+                else
+                {
+                    Font = new Font(txtbTextSite.SelectionFont, FontStyle.Italic);
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (txtbTextSite.SelectedText != null)
+            {
+                if (txtbTextSite.SelectionFont.Underline)
+                {
+                    txtbTextSite.SelectionFont = new Font(txtbTextSite.SelectionFont, FontStyle.Regular);
+                }
+                else
+                {
+                    txtbTextSite.SelectionFont = new Font(txtbTextSite.SelectionFont, FontStyle.Underline);
+                }
+            }
+            else
+            {
+                if (Font.Underline)
+                {
+                    Font = new Font(txtbTextSite.SelectionFont, FontStyle.Regular);
+                }
+                else
+                {
+
+                    Font = new Font(txtbTextSite.SelectionFont, FontStyle.Underline);
+
+                }
+            }
+        }
+
+        private void loadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (DialogResult.OK == ofd.ShowDialog())
+            {
+                txtbTextSite.LoadFile(ofd.FileName);
+                customPass = ofd.FileName;
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (customPass != null)
+            {
+                txtbTextSite.SaveFile(customPass);
+            }
+            else
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                if (DialogResult.OK == ofd.ShowDialog())
+                {
+                    txtbTextSite.SaveFile(ofd.FileName);
+                }
+
+
+            }
+
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (DialogResult.OK == ofd.ShowDialog())
+            {
+                txtbTextSite.SaveFile(ofd.FileName);
+            }
+        }
+
+
+
+
+
+
+
+    }
+}
